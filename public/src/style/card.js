6288dfe83daba1499846488e0f1db2d9fa40e78a
George Spake <gpspake@gmail.com>
2017-09-10 11:12:40 -0500

    card css update to fix layout

--- public/src/style/card.js
+++ public/src/style/card.js
@@ -4,34 +4,17 @@ const width = Dimensions.get('window').width; //full width
 const height = Dimensions.get('window').height; //full height
 
 export default StyleSheet.create({
-  hero: {
-    paddingTop: 50,
-    backgroundColor: '#66b34e',
+  baseText: {
+    fontFamily: 'Cochin',
   },
-  lookingFor: {
-    backgroundColor: '#e3da74',
-    flex: 1,
-  },
-  profileData: {
-    backgroundColor: '#66b34e',
-  },
-  actionImageButton: {
-    padding: 10,
-    height: 90,
-  },
-  actionButton: {
-    padding: 10,
-    marginTop: 5,
-  },
+
   profile: {
     flex: 1,
-    flexDirection: 'column',
-
+    backgroundColor: 'tomato',
   },
   card: {
-    flex: 3,
+    flex: 1,
     width: width,
-    alignSelf: 'stretch',
     alignItems: 'center',
     borderRadius: 5,
     overflow: 'hidden',
@@ -41,7 +24,7 @@ export default StyleSheet.create({
     elevation: 1,
   },
   actions: {
-    flex: 1,
+    flex: .10,
     backgroundColor: '#e3da74',
     alignSelf: 'stretch',
     justifyContent: 'center',
@@ -56,12 +39,31 @@ export default StyleSheet.create({
   },
   text: {
     fontSize: 20,
-    paddingTop: 10,
-    paddingBottom: 5,
+    //paddingTop: 10,
+    //paddingBottom: 5,
+    color: 'black'
   },
   noMoreCards: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  hero: {
+    paddingTop: 50,
+    backgroundColor: '#66b34e',
+  },
+  lookingFor: {
+    backgroundColor: '#e3da74',
+    //flex: 1,
+  },
+  profileData: {
+    //backgroundColor: '#66b34e',
+  },
+  actionImageButton: {
+    padding: 10,
+  },
+  actionButton: {
+    padding: 10,
+    marginTop: 5,
   }
 })
