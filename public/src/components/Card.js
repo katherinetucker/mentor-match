87e3499acef78ac875b11fb6706fc28f49ea940e
George Spake <gpspake@gmail.com>
2017-09-10 10:09:11 -0500

    clean up unused imports

--- public/src/components/Card.js
+++ public/src/components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, Image, Dimensions} from 'react-native';
+import {Text, View, Image} from 'react-native';
 
 import styles from '../style/card.js'
 
--- public/src/components/NoCards.js
+++ public/src/components/NoCards.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, Image, Dimensions, Button} from 'react-native';
+import {Text, View} from 'react-native';
 
 import styles from '../style/card.js'
 
